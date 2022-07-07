5f8ee32e65899dc43a716a9017f188f30f457b39
chenk <hen.keinan@gmail.com>
2022-07-07 17:38:41 +0300

    feat: k8s outdated api version
    Signed-off-by: chenk <hen.keinan@gmail.com>

---
+++ go.mod
@@ -0,0 +1,12 @@
+module k8s-outdated
+
+go 1.18
+
+require (
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/hashicorp/go-version v1.6.0 // indirect
+	github.com/kataras/tablewriter v0.0.0-20180708051242-e063d29b7c23 // indirect
+	github.com/lensesio/tableprinter v0.0.0-20201125135848-89e81fc956e7 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/hashicorp/go-version"
+	"github.com/lensesio/tableprinter"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type K8sAPI struct {
+	Kind              string `header:"k8s api"`
+	DeprecatedVersion string `header:"deprecated Version"`
+	RemovedVersion    string `header:"removed Version"`
+}
+
+func main() {
+	baseURL := "https://raw.githubusercontent.com/kubernetes/kubernetes"
+	fileURL := "api/openapi-spec/swagger.json"
+	k8sVer := "1.17.7"
+	kVer := getRelevantK8sVersions(k8sVer)
+	mapList := make(map[string]map[string]interface{}, 0)
+	gavMap := make(map[string]k8sObject)
+	mDetials := versionToDetails(kVer, baseURL, fileURL, mapList, gavMap)
+	apis := make([]K8sAPI, 0)
+	for key, val := range mDetials {
+		fmt.Println(fmt.Sprintf("Kubernetes Version: %s", key))
+		for _, data := range val {
+			if len(data.Deprecated) == 0 && len(data.Removed) == 0 {
+				continue
+			}
+			if len(data.Gav.Kind) == 0 || len(data.Gav.Version) == 0 || len(data.Gav.Group) == 0 {
+				continue
+			}
+			apis = append(apis, K8sAPI{Kind: fmt.Sprintf("%s.%s.%s", data.Gav.Group, data.Gav.Version, data.Gav.Kind), DeprecatedVersion: data.Deprecated, RemovedVersion: data.Removed})
+		}
+	}
+	tableprinter.Print(os.Stdout, apis)
+}
+
+func versionToDetails(kVer []string, baseURL string, fileURL string, mapList map[string]map[string]interface{}, gavMap map[string]k8sObject) map[string]map[string]k8sObject {
+	mapK8s := make(map[string]map[string]k8sObject)
+	for _, kv := range kVer {
+		url := fmt.Sprintf("%s/%s/%s", baseURL, kv, fileURL)
+		res, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		var apiMap map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&apiMap)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		mapList[kv] = apiMap
+		p := apiMap["definitions"]
+		for key, val := range p.(map[string]interface{}) {
+			mval := val.(map[string]interface{})
+			gav, ok := mval["x-kubernetes-group-version-kind"].(interface{})
+			b, err := json.Marshal(&gav)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			if !ok {
+				continue
+			}
+			var ga []Gav
+			err = json.Unmarshal(b, &ga)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			if len(ga) == 0 {
+				continue
+			}
+			desc, ok := mval["description"].(string)
+			if !ok {
+				continue
+			}
+			var dep string
+			var rem string
+			lower := strings.ToLower(desc)
+			if strings.Contains(lower, "deprecated in") {
+				dIndex := strings.Index(lower, "deprecated in")
+				ndes := lower[dIndex+13:]
+				sndes := strings.Split(strings.TrimPrefix(ndes, " "), " ")
+				dep = strings.TrimSuffix(strings.TrimSuffix(sndes[0], ","), ".")
+			}
+			if strings.Contains(lower, "removal in") {
+				dIndex := strings.Index(lower, "removal in")
+				ndes := lower[dIndex+11:]
+				sndes := strings.Split(strings.TrimPrefix(ndes, " "), " ")
+				rem = strings.TrimSuffix(strings.TrimSuffix(sndes[0], ","), ".")
+			}
+			object := k8sObject{Description: desc, Gav: ga[0], Deprecated: dep, Removed: rem}
+			gavMap[key] = object
+
+		}
+		mapK8s[kv] = gavMap
+		break
+	}
+	return mapK8s
+}
+
+type k8sObject struct {
+	Description string
+	Deprecated  string
+	Removed     string
+	Gav         Gav
+}
+
+type Gav struct {
+	Group   string `json:"group"`
+	Version string `json:"version"`
+	Kind    string `json:"kind"`
+}
+
+func getRelevantK8sVersions(k8sVer string) []string {
+	r, err := http.Get("https://api.github.com/repos/kubernetes/kubernetes/git/refs/tags")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	var refs []Ref
+	err = json.NewDecoder(r.Body).Decode(&refs)
+	if err != nil {
+		fmt.Println(err)
+	}
+	v1, err := version.NewVersion(k8sVer)
+	kVer := make([]string, 0)
+	for _, r := range refs {
+		if strings.Contains(r.Ref, "-rc") ||
+			strings.Contains(r.Ref, "-alpha") ||
+			strings.Contains(r.Ref, "-beta") {
+			continue
+		}
+		v := strings.Replace(r.Ref, "refs/tags/", "", -1)
+		v2, err := version.NewVersion(strings.Replace(v, "v", "", -1))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if v1.LessThanOrEqual(v2) {
+			kVer = append(kVer, v)
+		}
+	}
+	return kVer
+}
+
+type Ref struct {
+	Ref    string `json:"ref"`
+	NodeId string `json:"node_id"`
+	Url    string `json:"url"`
+}
